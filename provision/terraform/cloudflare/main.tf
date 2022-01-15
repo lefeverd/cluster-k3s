32e8c542194ba1d05d4b81886f0fd63519f5bc4f
David Lefever <lefever.d@gmail.com>
2022-01-15 16:40:45 +0100

    Added argocd.

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -118,3 +118,12 @@ resource "cloudflare_record" "echoserver" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "simplebookmarks" {
+  name    = "simplebookmarks"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}

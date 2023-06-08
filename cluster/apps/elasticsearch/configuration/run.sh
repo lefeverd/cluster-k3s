99767fb48bde76622d88ddd08350c23adc4b7623
David Lefever <lefever.d@gmail.com>
2023-06-08 22:36:16 +0200

    fix elasticsearch configuration script

--- cluster/apps/elasticsearch/configuration/run.sh
+++ cluster/apps/elasticsearch/configuration/run.sh
@@ -3,13 +3,13 @@
 ES_URL=https://localhost:9200
 
 echo "Creating ILM policy for backup logs"
-curl -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_ilm/policy/backup-logs-policy" -H 'Content-Type: application/json' -d@/configuration/ilm-backup-logs.json
+curl -k -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_ilm/policy/backup-logs-policy" -H 'Content-Type: application/json' -d@/configuration/ilm-backup-logs.json
 
 echo "Creating mappings for backup logs"
-curl -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_component_template/backup-logs-mappings" -H 'Content-Type: application/json' -d@/configuration/mapping-backup-logs.json
+curl -k -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_component_template/backup-logs-mappings" -H 'Content-Type: application/json' -d@/configuration/mapping-backup-logs.json
 
 echo "Creating settings for backup logs"
-curl -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_component_template/backup-logs-settings" -H 'Content-Type: application/json' -d@/configuration/settings-backup-logs.json
+curl -k -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_component_template/backup-logs-settings" -H 'Content-Type: application/json' -d@/configuration/settings-backup-logs.json
 
 echo "Creating index template for backup logs"
-curl -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_index_template/backup-logs-template" -H 'Content-Type: application/json' -d@/configuration/template-backup-logs.json
+curl -k -u \"elastic:$ELASTIC_PASSWORD\" -XPUT "$ES_URL/_index_template/backup-logs-template" -H 'Content-Type: application/json' -d@/configuration/template-backup-logs.json
